ebe2a1e4e943132e87668725d0523dd804a9a062
Soniabensaad <5687@holbertonstudents.com>
2023-04-20 19:10:56 +0200

    hello api

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,12 +5,13 @@ import os
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
+
 """
 create a variable app, instance of Flask
 import storage from models
 import app_views from api.v1.views
-register the blueprint app_views to your Flask instance app
-declare a method to handle @app.teardown_appcontext that calls storage.close()
+register the blueprint app_views to your Flask ins
 inside if __name__ == "__main__":, run your Flask server (variable app) with:
 host = environment variable HBNB_API_HOST or 0.0.0.0 if not defined
 port = environment variable HBNB_API_PORT or 5000 if not defined
@@ -19,13 +20,14 @@ threaded=True
 
 # Create Flask instance
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 # Register blueprints
 app.register_blueprint(app_views)
 
 # Teardown app context
 @app.teardown_appcontext
-def teardown_app_context(error):
+def teardown_app_context(exception):
     
     storage.close()
 
