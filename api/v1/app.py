3b2621b029c190f02c585c9d3b400a4510cde277
Soniabensaad <5687@holbertonstudents.com>
2023-04-20 19:19:01 +0200

    hello api

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,33 +7,21 @@ from models import storage
 from api.v1.views import app_views
 from flask_cors import CORS
 
-"""
-create a variable app, instance of Flask
-import storage from models
-import app_views from api.v1.views
-register the blueprint app_views to your Flask ins
-inside if __name__ == "__main__":, run your Flask server (variable app) with:
-host = environment variable HBNB_API_HOST or 0.0.0.0 if not defined
-port = environment variable HBNB_API_PORT or 5000 if not defined
-threaded=True
-"""
-
-# Create Flask instance
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
-# Register blueprints
 app.register_blueprint(app_views)
 
-# Teardown app context
 @app.teardown_appcontext
 def teardown_app_context(exception):
     
     storage.close()
+"""
+create a variable app, instance of Flask
+import storage from models
 
+"""
 if __name__ == "__main__":
     host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
     port = os.environ.get("HBNB_API_PORT", 5000)
-
-   
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """import app_views from api.v1.views
 """
 from api.v1.views import app_views
