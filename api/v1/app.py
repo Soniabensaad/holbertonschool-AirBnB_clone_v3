7d74f4f44f6df9356a62335eb6685a8682db21db
Soniabensaad <5687@holbertonstudents.com>
2023-04-20 19:02:17 +0200

    hello api

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,17 @@ import os
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+"""
+create a variable app, instance of Flask
+import storage from models
+import app_views from api.v1.views
+register the blueprint app_views to your Flask instance app
+declare a method to handle @app.teardown_appcontext that calls storage.close()
+inside if __name__ == "__main__":, run your Flask server (variable app) with:
+host = environment variable HBNB_API_HOST or 0.0.0.0 if not defined
+port = environment variable HBNB_API_PORT or 5000 if not defined
+threaded=True
+"""
 
 # Create Flask instance
 app = Flask(__name__)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 from api.v1.views import app_views
 from flask import jsonify
-
+"""
+import app_views from api.v1.views
+create a route /status on the object app_views that returns a JSON
+"""
 @app_views.route('/status', strict_slashes=False)
 def status():
     return jsonify(status="OK")
