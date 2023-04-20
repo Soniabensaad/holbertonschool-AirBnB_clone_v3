57fcb2c8d2a62f122badd3b129a7b143f2825b8e
Soniabensaad <5687@holbertonstudents.com>
2023-04-20 19:14:15 +0200

    hello api

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,3 +6,6 @@ from flask import Blueprint
 from api.v1.views.index import *
 """mport Blueprint from flask doc"""
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
+
