acc47fb917ac42a73dd2f946188ece465b1e62e4
Soniabensaad <5687@holbertonstudents.com>
2023-04-20 19:27:57 +0200

    hello api

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,27 +1,37 @@
 #!/usr/bin/python3
-"""create a variable app, instance of Flask
 """
-import os
-from flask import Flask
+create a variable app, instance of Flask
+"""
+
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-
 app.register_blueprint(app_views)
 
+
+@app.route("/")
+def hello():
+    return ("hello")
+
+
 @app.teardown_appcontext
-def teardown_app_context(exception):
-    
+def teardown_db(exception):
     storage.close()
-"""
-create a variable app, instance of Flask
-import storage from models
+
 
 """
+create a variable app, instance of Flask
+"""
+
+
+
+
 if __name__ == "__main__":
-    host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
-    port = os.environ.get("HBNB_API_PORT", 5000)
+    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    port = os.getenv("HBNB_API_PORT", 5000)
     app.run(host=host, port=port, threaded=True)
