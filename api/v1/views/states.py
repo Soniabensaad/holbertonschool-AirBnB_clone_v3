e008c942e76c186ef10c6f7ce24525a09842c863
Soniabensaad <5687@holbertonstudents.com>
2023-04-21 19:51:08 +0200

    hello api

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+"""
+create a new view for staye
+objects that handles all default RESTFul API actions"""
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from models import storage
