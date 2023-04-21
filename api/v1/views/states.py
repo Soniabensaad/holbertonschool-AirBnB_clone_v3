db418eee5155df6d31dea49143eebb887d0a0386
Soniabensaad <5687@holbertonstudents.com>
2023-04-21 14:18:13 +0200

    hello api

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -6,13 +6,22 @@ from flask import Flask, jsonify,  request
 from models import storage
 from models.state import State
 import models
-@app_views.route('/states', methods=['GET'], strict_slashes=False)
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get(self, state_id):
-        """retrieve one object:"""
-        if state_id:
-            state = State.query.get_or_404(state_id)
-            return jsonify(state.to_dict())
-        else:
-            states = State.query.all()
-            return jsonify([state.to_dict() for state in states])
-       
+    """retrieve one object:"""
+    if state_id:
+        state = State.query.get_or_404(state_id)
+        return jsonify(state.to_dict())
+    else:
+        states = State.query.all()
+        return jsonify([state.to_dict() for state in states])
+
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete(self, state_id):
+    """delete one by id"""
+    state = State.query.get_or_404(state_id)
+    state.delete()
+    return jsonify({}), 200
+
+    
