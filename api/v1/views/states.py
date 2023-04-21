2560de8651c11f44d6c1b9d5ee0b67505e89107b
Soniabensaad <5687@holbertonstudents.com>
2023-04-21 15:19:20 +0200

    hello api

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,55 +1,45 @@
 #!/usr/bin/python3
-"""Create a new view for State objects"""
-
-from flask import jsonify, abort, request
-from api.v1.views import app_views, storage
+"""
+Create a new view for State objects that handles
+all default RESTFul API actions:
+"""
+from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
+from models import storage
 from models.state import State
+import models
 
 
-@app_views.route("/states", methods=["GET"], strict_slashes=False)
-def state_get_all():
-    """
-    retrieves all State objects
-    :return: json of all states
-    """
-    state_list = []
-    state_obj = storage.all("State")
-    for obj in state_obj.values():
-        state_list.append(obj.to_json())
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def status():
+    slist = []
+    states = storage.all(State).values()
+    for state in states:
+        slist.append(state.to_dict())
+    return jsonify(slist)
 
-    return jsonify(state_list)
 
-    
-@app_views.route("/states/<state_id>",  methods=["GET"], strict_slashes=False)
-def state_by_id(state_id):
-    """
-    gets a specific State object by ID
-    :param state_id: state object id
-    :return: state obj with the specified id or error
-    """
-
-    fetched_obj = storage.get("State", str(state_id))
-
-    if fetched_obj is None:
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def get_state(state_id):
+    """Retrieves a State object"""
+    states = storage.all(State)
+    key = "State."+state_id
+    if key not in states:
         abort(404)
+    a = states[key]
+    return jsonify(a.to_dict())
 
-    return jsonify(fetched_obj.to_json())
 
-@app_views.route("/states/<state_id>", methods=["DELETE"],
+@app_views.route('/states/<state_id>', methods=['DELETE'],
                  strict_slashes=False)
-def state_delete_by_id(state_id):
-    """
-    deletes State by id
-    :param state_id: state object id
-    :return: empty dict with 200 or 404 if not found
-    """
-
-    fetched_obj = storage.get("State", str(state_id))
-
-    if fetched_obj is None:
+def delete_state(state_id):
+    states = storage.all(State)
+    key = "State."+state_id
+    if key not in states:
         abort(404)
-
-    storage.delete(fetched_obj)
+    a = states[key]
+    storage.delete(a)
     storage.save()
+    return jsonify({}), 200
 
-    return jsonify({})
+
